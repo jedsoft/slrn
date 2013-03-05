dd57735827971e73893fc44dbffe083596dfd55d
John E. Davis <davis@space.mit.edu>
2013-03-04 21:57:32 -0500

    pre1.0.2 change 6: Updated the FSF address

--- src/version.h
+++ src/version.h
@@ -16,10 +16,10 @@
 
  You should have received a copy of the GNU General Public License along
  with this program; if not, write to the Free Software Foundation, Inc.,
- 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 */
 
-#define SLRN_VERSION_STRING "pre1.0.2-5"
+#define SLRN_VERSION_STRING "pre1.0.2-6"
 #define SLRN_VERSION 10020
 
 #ifndef SLRNPULL_CODE
